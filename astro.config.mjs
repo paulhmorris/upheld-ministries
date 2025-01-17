7bf4ef04f04c36aa02d9ecaf6e520531b4a1d36f
Paul Morris <paulh.morris@gmail.com>
2025-01-17 16:48:22 -0600

    Finish quote carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,31 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const iconsPath = "node_modules/@shoelace-style/shoelace/dist/assets/icons";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: /\/assets\/icons\/(.+)/,
+          replacement: `${iconsPath}/$1`,
+        },
+      ],
+    },
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: iconsPath,
+            dest: "assets",
+          },
+        ],
+      }),
+    ],
+  },
   integrations: [tailwind({ applyBaseStyles: false }), partytown(), sitemap(), icon()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,25 @@
+import colors from "tailwindcss/colors";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      white: colors.white,
+      black: colors.black,
+      gray: colors.gray,
+      brand: {
+        "dark-grey": "#313D41",
+        "dark-teal": "#476c77",
+        orange: "#EFB071",
+        blurple: "#E0E2ED",
+      },
+    },
     extend: {
       fontFamily: {
         serif: ["'EB Garamond Variable'", ...defaultTheme.fontFamily.serif],
         orpheus: ["'Orpheus Pro'", ...defaultTheme.fontFamily.serif],
-      },
-      colors: {
-        brand: {
-          "dark-grey": "#313D41",
-          orange: "#EFB071",
-          blurple: "#E0E2ED",
-        },
       },
     },
   },
